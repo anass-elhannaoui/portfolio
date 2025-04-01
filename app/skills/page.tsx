9b2abd53effd7ad0f14a615ddc718c630fef5bc7
Anass El Hannaoui <154076278+anass-elhannaoui@users.noreply.github.com>
2025-04-01 20:08:38 +0000

    Update skills

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -124,7 +124,7 @@ export default function Skills() {
   ];
 
   return (
-    <div className="min-h-screen pt-24 bg-background">
+    <div className="min-h-screen pt-14 pb-10 bg-background">
       <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
