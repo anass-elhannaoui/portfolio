fa6e08d886c79924c1e5d62a4873d8f00d1dc7c5
anass-elhannaoui <anass.elhannaoui23@ump.ac.ma>
2025-09-10 17:53:12 +0100

    Setup GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
+  eslint: { ignoreDuringBuilds: true },
   images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
-
